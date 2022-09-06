a4d3628793851e44c4f1a4d6bdf96c16af432c2f
Phil Bedford <phil.bedford@protocol.ai>
2022-09-06 15:23:56 -0700

    added collections.js, which tests: (1) creating a collection, (2) adding a file to that collection, and (3) adding a file to that collection with a path. Also changed index.js to use the upload.estuary endpoint

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const FormData = require("form-data");
 const fetch = require("node-fetch");
 
-const key = `YOUR API KEY`;
+const key = `YOUR_API_KEY`;
 
 var form = new FormData();
 const path = `${__dirname}/nature-6.mp4`;
@@ -12,7 +12,7 @@ form.append("data", fs.createReadStream(path));
 const headers = form.getHeaders();
 console.log(headers);
 
-fetch("https://shuttle-5.estuary.tech/content/add", {
+fetch("https://upload.estuary.tech/content/add", {
   method: "POST",
   body: form,
   headers: {
